1d7ce438e24232a5bca71533eb3fc832984b65c0
Paulo Vieira <paulo92vieira@gmail.com>
2024-09-23 17:37:13 +0100

    sending things

---
+++ app/donation/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/index.css";
+const page = () => {
+  return (
+    <div>
+      <div className="Total">VALOR TOTAL DONATION</div>
+      <div className="container-cards">
+        <div className="Cards">Cards</div>
+      </div>
+      <div className="Testemunha"> SLIDE TESTEMUNHA</div>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/pets/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/index.css";
+
+const page = () => {
+  return (
+    <div className="container">
+      <div className="sidebar">FILTROS</div>
+      <div className="banner">TEXT DOS PETS BONITO</div>
+    </div>
+  );
+};
+
+export default page;
