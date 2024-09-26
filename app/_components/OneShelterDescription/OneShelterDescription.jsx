848a1765d25571b3e5f0d9f23b8d39b4eeea8efe
Vitoria <vitoriaac19@gmail.com>
2024-09-26 20:04:38 +0100

    map done

--- app/_components/OneShelterDescription/OneShelterDescription.jsx
+++ app/_components/OneShelterDescription/OneShelterDescription.jsx
@@ -1,24 +1,19 @@
 import "./styles/index.css";
 
-import React from 'react'
+import React from "react";
 
-function OneShelterDescription({shelterHistory, shelterSocialMedia}) {
+function OneShelterDescription({ shelterHistory }) {
   return (
     <div className="one-shelter-container-description">
-        <h2 className="one-shelter-description-title">História</h2>
-        <p className="one-shelter-description-text">{shelterHistory}</p>
-        <div className="one-shelter-social-media">
-            
-            <div className="one-shelter-social-media-buttons">
-                <p>Contacte-nos</p>
-                <span>{shelterSocialMedia}</span>
-            </div>
-            <div className="one-shelter-voluteer">
-                <button className="one-shelter-voluteer-button">Voluntariar</button>
-            </div>
+      <h2 className="one-shelter-description-title">História</h2>
+      <p className="one-shelter-description-text">{shelterHistory}</p>
+      <div className="one-shelter-social-media">
+        <div className="one-shelter-voluteer">
+          <button className="one-shelter-voluteer-button">Voluntariar</button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default OneShelterDescription
+export default OneShelterDescription;
--- app/shelter/[shelterId]/page.jsx
+++ app/shelter/[shelterId]/page.jsx
@@ -4,8 +4,11 @@ import "./styles/index.css";
 import ShelterImgTop from "../../_components/ShelterImgTop/ShelterImgTop";
 import ShelterCarousel from "@/app/_components/ShelterCarousel/ShelterCarousel";
 import OneShelterDescription from "@/app/_components/OneShelterDescription/OneShelterDescription";
+import Map from "@/app/_components/Map/Map";
+import ShelterSocialMedia from "@/app/_components/ShelterSocialMedia/ShelterSocialMedia";
 
 const page = () => {
+  const position = [41.1517, -8.6099];
   const shelterInformations = [
     {
       imageShelterLogo:
@@ -15,8 +18,9 @@ const page = () => {
         "https://storage.googleapis.com/mindera-cms-media-uploads/Becoming_A_Back_End_Developer_1_7cbf5900a7/Becoming_A_Back_End_Developer_1_7cbf5900a7.png",
         "https://avatars.githubusercontent.com/u/43017609?s=280&v=4",
       ],
-      shelterHistory: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Curabitur vitae purus nec libero varius dictum. Praesent eget dolor ac mi malesuada interdum. Sed suscipit diam id orci efficitur, eget commodo risus tincidunt. Cras viverra justo sit amet est venenatis, vitae facilisis mi fermentum. Ut dictum sapien vel tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Curabitur vitae purus nec libero varius dictum. Praesent eget dolor ac mi malesuada interdum. Sed suscipit diam id orci efficitur, eget commodo risus tincidunt. Cras viverra justo sit amet est venenatis, vitae facilisis mi fermentum. Ut dictum sapien vel tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Curabitur vitae purus nec libero varius dictum. Praesent eget dolor ac mi malesuada interdum. Sed suscipit diam id orci efficitur, eget commodo risus tincidunt. Cras viverra justo sit amet est venenatis, vitae facilisis mi fermentum. Ut dictum sapien vel tincidunt.Praesent eget dolor ac mi malesuada interdum. Sed suscipit diam id orci efficitur, eget commodo risus tincidunt. Cras viverra justo sit amet est venenatis, vitae facilisis mi fermentum. Ut dictum sapien vel tincidunt.",
-      shelterSocialMedia:"",
+      shelterHistory:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Curabitur vitae purus nec libero varius dictum. Praesent eget dolor ac mi malesuada interdum. Sed suscipit diam id orci efficitur, eget commodo risus tincidunt. Cras viverra justo sit amet est venenatis, vitae facilisis mi fermentum. Ut dictum sapien vel tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Curabitur vitae purus nec libero varius dictum. Praesent eget dolor ac mi malesuada interdum. Sed suscipit diam id orci efficitur, eget commodo risus tincidunt. Cras viverra justo sit amet est venenatis, vitae facilisis mi fermentum. Ut dictum sapien vel tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Curabitur vitae purus nec libero varius dictum. Praesent eget dolor ac mi malesuada interdum. Sed suscipit diam id orci efficitur, eget commodo risus tincidunt. Cras viverra justo sit amet est venenatis, vitae facilisis mi fermentum. Ut dictum sapien vel tincidunt.Praesent eget dolor ac mi malesuada interdum. Sed suscipit diam id orci efficitur, eget commodo risus tincidunt. Cras viverra justo sit amet est venenatis, vitae facilisis mi fermentum. Ut dictum sapien vel tincidunt.",
+      shelterSocialMedia: "",
     },
   ];
 
@@ -43,6 +47,17 @@ const page = () => {
             shelterSocialMedia={shelterInformation.shelterSocialMedia}
           />
         ))}
+      </div>
+
+      <div className="container-shelter-map">
+        <div className="one-shelter-map">
+          <div className="one-shelter-map-map">
+            <Map position={position}/>
+          </div>
+          <div className="container-one-shelter-social-media">
+            <ShelterSocialMedia />
+          </div>
+        </div>
       </div>
     </>
   );
