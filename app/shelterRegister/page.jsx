70dd4499078c0e45523bc248c65314b5b30d89fc
Rui Sousa <150975299+ruitiago23@users.noreply.github.com>
2024-09-26 21:11:04 +0100

    make a registration form for the shelter and style it

--- app/shelterRegister/page.jsx
+++ app/shelterRegister/page.jsx
@@ -1,9 +1,156 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import "./styles/index.css";
 
 const page = () => {
+  const [name, setName] = useState("");
+  const [nif, setNif] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [cellNumber, setCellNumber] = useState("");
+  const [size, setSize] = useState("");
+  const [successMessage, setSuccessMessage] = useState(false);
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleNif = (event) => {
+    setNif(event.target.value);
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleAddress = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const handlePostalCode = (event) => {
+    setPostalCode(event.target.value);
+  };
+
+  const handleCellNumber = (event) => {
+    setCellNumber(event.target.value);
+  };
+
+  const handleSize = (event) => {
+    setSize(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(
+      name,
+      nif,
+      email,
+      password,
+      address,
+      postalCode,
+      cellNumber,
+      size
+    );
+  };
+  const handleRegister = async () => {
+    const response = await fetch(
+      "",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          nif: nif,
+          email: email,
+          address: address,
+          postalCode: postalCode,
+          cellNumber: cellNumber,
+          size: size,
+        }),
+      }
+    );
+
+    console.log(response);
+    if (response.ok) {
+      setSuccessMessage(true);
+    }
+  };
+
   return (
-    <div>page</div>
-  )
+    <div className="shelter-register-container">
+      <div className="shelter-register-container-register">
+        <div className="shelter-register-title-register">Registe a Sua Assosiação</div>
+
+        <div className="shelter-register-container-register-input">
+          <input
+            type="text"
+            placeholder="Nome"
+            onChange={handleName}
+            value={name}
+            required
+          />
+          <input
+            type="number"
+            placeholder="NIF"
+            onChange={handleNif}
+            value={nif}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={handleEmail}
+            value={email}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Direção"
+            onChange={handleAddress}
+            value={address}
+            required
+          />
+          <input
+            type="number"
+            placeholder="Código Postal"
+            onChange={handlePostalCode}
+            value={postalCode}
+            required
+          />
+          <input
+            type="number"
+            placeholder="Telefone"
+            onChange={handleCellNumber}
+            value={cellNumber}
+          />
+          <input
+            type="number"
+            placeholder="Tamanho (m²)"
+            onChange={handleCellNumber}
+            value={cellNumber}
+          />
+        </div>
+
+        <div className="shelter-register-container-button-register">
+          <button
+            type="submit"
+            onSubmit={handleSubmit}
+            onClick={handleRegister}
+          >
+            Registar
+          </button>
+        </div>
+        {successMessage && (
+          <div className="shelter-register-success-message">
+            <p>Registado com sucesso!</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default page
