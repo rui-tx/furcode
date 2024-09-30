efe95cd8366595232f8313c61446fcb90e682d2f
Vitoria <vitoriaac19@gmail.com>
2024-09-30 12:04:11 +0100

    shelter infinity scroll done

--- app/_components/ShelterGalleryComponents/ShelterGallery/ShelterGallery.jsx
+++ app/_components/ShelterGalleryComponents/ShelterGallery/ShelterGallery.jsx
@@ -1,20 +1,49 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./styles/index.css";
 import ShelterGalleryCard from "../ShelterGalleryCard/ShelterGalleryCard";
 import ShelterBanner from "../ShelterBanner/ShelterBanner";
 
 const ShelterGallery = () => {
   const [shelters, setShelters] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const sheltersPerPage = 10;
+
+  const fetchShelters = useCallback(async () => {
+    try {
+      const ENDPOINT = "api/shelter";
+      const URL_CONFIGURED = `${ENDPOINT}?limit=${sheltersPerPage}&page=${currentPage}&order=DESC`;
+      const response = await fetch(URL_CONFIGURED);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setShelters((prevShelters) => [...prevShelters, ...data]);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching shelters:", error);
+    }
+  }, [currentPage]);
 
   useEffect(() => {
-    const fetchShelters = async () => {
-      const result = await fetch("api/shelter");
-      const data = await result.json();
-      setShelters(data);
-      console.log(data);
-    };
     fetchShelters();
+  }, [fetchShelters]);
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries.some((entry) => entry.isIntersecting)) {
+        setCurrentPage((prevPage) => prevPage + 1);
+      }
+    });
+
+    const sentinel = document.querySelector('.sentinel');
+    if (sentinel) {
+      intersectionObserver.observe(sentinel);
+    }
+
+    return () => {
+      intersectionObserver.disconnect();
+    };
   }, []);
 
   return (
@@ -27,6 +56,7 @@ const ShelterGallery = () => {
           <ShelterGalleryCard key={shelter.id} shelter={shelter} />
         ))}
       </div>
+      <div className="sentinel"></div>
     </div>
   );
 };
