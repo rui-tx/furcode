d7b0674bbe7617d06656f3341799c857ab3c17f3
Rui Sousa <150975299+ruitiago23@users.noreply.github.com>
2024-09-30 11:19:56 +0100

    all fixed

--- app/shelterRegister/page.jsx
+++ app/shelterRegister/page.jsx
@@ -3,145 +3,153 @@ import React, { useState } from "react";
 import "./styles/index.css";
 
 const page = () => {
-  const [name, setName] = useState("");
-  const [nif, setNif] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [cellNumber, setCellNumber] = useState("");
-  const [size, setSize] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    nif: "",
+    email: "",
+    address: "",
+    postalCode: "",
+    cellPhone: "",
+    size: "",
+  });
   const [successMessage, setSuccessMessage] = useState(false);
 
-  const handleName = (event) => {
-    setName(event.target.value);
+  const handleChange = async (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  const handleNif = (event) => {
-    setNif(event.target.value);
-  };
-
-  const handleEmail = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleAddress = (event) => {
-    setAddress(event.target.value);
-  };
-
-  const handlePostalCode = (event) => {
-    setPostalCode(event.target.value);
-  };
-
-  const handleCellNumber = (event) => {
-    setCellNumber(event.target.value);
-  };
-
-  const handleSize = (event) => {
-    setSize(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(
-      name,
-      nif,
-      email,
-      password,
-      address,
-      postalCode,
-      cellNumber,
-      size
-    );
-  };
-  const handleRegister = async () => {
-    const response = await fetch("", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        nif: nif,
-        email: email,
-        address: address,
-        postalCode: postalCode,
-        cellNumber: cellNumber,
-        size: size,
-      }),
-    });
+    try {
+      const response = await fetch("", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-    console.log(response);
-    if (response.ok) {
-      setSuccessMessage(true);
+      if (response.ok) {
+        setSuccessMessage(true);
+        setFormData({
+          name: "",
+          nif: "",
+          email: "",
+          address: "",
+          postalCode: "",
+          cellPhone: "",
+          size: "",
+        });
+      } else {
+        console.error("Registration Failed");
+      }
+    } catch (error) {
+      console.error("Error submiting form:", error);
     }
   };
 
   return (
-    <div className="shelter-register-container">
-      <h2 className="shelter-register-title">Registe a Sua Associação</h2>
-      <div className="shelter-register-text"></div>
-      <div className="shelter-register-container-register">
-        <div className="shelter-register-container-input">
-          <input
-            type="text"
-            placeholder="Nome"
-            onChange={handleName}
-            value={name}
-            required
-          />
-          <input
-            type="number"
-            placeholder="NIF"
-            onChange={handleNif}
-            value={nif}
-            required
-          />
-          <input
-            type="text"
-            placeholder="Email"
-            onChange={handleEmail}
-            value={email}
-            required
-          />
-          <input
-            type="text"
-            placeholder="Direção"
-            onChange={handleAddress}
-            value={address}
-            required
-          />
-          <input
-            type="number"
-            placeholder="Código Postal"
-            onChange={handlePostalCode}
-            value={postalCode}
-            required
-          />
-          <input
-            type="number"
-            placeholder="Telefone"
-            onChange={handleCellNumber}
-            value={cellNumber}
-            required
-          />
-          <input
-            type="number"
-            placeholder="Tamanho (m²)"
-            onChange={handleSize}
-            value={size}
-            required
-          />
+    <div className="page-container">
+      <div className="shelter-register-container">
+        <h2 className="shelter-register-title">Registe a Sua Associação</h2>
+        <div className="shelter-register-content-wrapper">
+          <div className="shelter-register-content">
+            <div className="shelter-register-text">
+              <p>
+                Junte-se à Furcode e faça parte de uma comunidade dedicada a
+                encontrar lares para os animais que mais precisam.
+              </p>
+              <br />
+              <p>
+                Ao registar a sua associação, estará a dar uma nova oportunidade
+                aos nossos amigos de quatro patas, ampliando o alcance das suas
+                ações e tornando o processo de adoção ainda mais simples e
+                acessível.
+              </p>
+              <br />
+              <p>
+                Queremos caminhar ao seu lado na missão de proporcionar novas
+                famílias a estes animais.
+              </p>
+              <br />
+              <p>Registe a sua associação e ajude-nos a fazer a diferença!</p>
+            </div>
+            <div className="shelter-register-form-wrapper">
+              <form
+                className="shelter-register-container-register"
+                onSubmit={handleSubmit}
+              >
+                <div className="shelter-register-container-input">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Nome"
+                    onChange={handleChange}
+                    value={formData.name}
+                    required
+                  />
+                  <input
+                    type="number"
+                    name="nif"
+                    placeholder="NIF"
+                    onChange={handleChange}
+                    value={formData.nif}
+                    required
+                  />
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                    value={formData.email}
+                    required
+                  />
+                  <input
+                    type="text"
+                    name="address"
+                    placeholder="Direção"
+                    onChange={handleChange}
+                    value={formData.address}
+                    required
+                  />
+                  <input
+                    type="number"
+                    name="postalCode"
+                    placeholder="Código Postal"
+                    onChange={handleChange}
+                    value={formData.postalCode}
+                    required
+                  />
+                  <input
+                    type="number"
+                    name="cellPhone"
+                    placeholder="Telefone"
+                    onChange={handleChange}
+                    value={formData.cellPhone}
+                    required
+                  />
+                  <input
+                    type="number"
+                    name="size"
+                    placeholder="Tamanho (m²)"
+                    onChange={handleChange}
+                    value={formData.size}
+                    required
+                  />
+                </div>
+
+                <div className="shelter-register-container-button">
+                  <button type="submit">Registar</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
 
-        <div className="shelter-register-container-button">
-          <button
-            type="submit"
-            onSubmit={handleSubmit}
-            onClick={handleRegister}
-          >
-            Registar
-          </button>
-        </div>
         {successMessage && (
           <div className="shelter-register-success-message">
             <p>Registado com sucesso!</p>
