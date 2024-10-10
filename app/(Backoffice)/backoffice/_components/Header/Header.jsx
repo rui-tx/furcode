d1e1a456eb7a6c30324ec20c654c5e703c260b88
Rui Sousa <150975299+ruitiago23@users.noreply.github.com>
2024-10-10 15:31:45 +0100

    name fix in backoffice

--- app/(Backoffice)/backoffice/_components/Header/Header.jsx
+++ app/(Backoffice)/backoffice/_components/Header/Header.jsx
@@ -3,13 +3,23 @@ import React from "react";
 import { Bell, User, LogOut } from "lucide-react";
 import "./styles/index.css";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/app/context/AuthContext";
 
-const Header = ({ userName }) => {
+const Header = () => {
   const router = useRouter();
+  const { user } = useAuth();
+
+  // Adicione uma verificação para evitar erros caso o usuário não esteja definido
+  const userName = user ? user.firstName : "Visitante";
 
   const handleLogout = () => {
+    // Aqui você pode querer limpar o localStorage e redirecionar para a página de login
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
     router.push("/");
   };
+
   return (
     <header className="backoffice-header">
       <div className="backoffice-header-logo">
